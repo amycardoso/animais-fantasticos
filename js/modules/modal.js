b384035e11c5a32ce65fdc261b166e3b7cd4b8f9
amycardoso <alice.cardoso10@gmail.com>
2019-11-27 09:58:05 -0500

    adicionado slide de fotos

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,11 +1,11 @@
-export default class initModal {
+export default class Modal {
   constructor(botaoAbrir, botaoFechar, containerModal) {
     this.botaoAbrir = document.querySelector(botaoAbrir);
     this.botaoFechar = document.querySelector(botaoFechar);
     this.containerModal = document.querySelector(containerModal);
 
     // bind this ao callback para
-    // fazer referencia ao objeto
+    // fazer referência ao objeto
     // da classe
     this.eventToggleModal = this.eventToggleModal.bind(this);
     this.cliqueForaModal = this.cliqueForaModal.bind(this);
@@ -29,7 +29,7 @@ export default class initModal {
     }
   }
 
-  // adiciona eventos aos elementos do model
+  // adiciona os eventos aos elementos do modal
   addModalEvents() {
     this.botaoAbrir.addEventListener('click', this.eventToggleModal);
     this.botaoFechar.addEventListener('click', this.eventToggleModal);
